5810aaed6c385d4e98d6685a9dde7aa104c05b3d
Alex Beregszaszi <alex@rtfs.hu>
2015-11-30 15:41:49 +0000

    Support multiple updates (as update is a stream)

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const hashLengths = [ 224, 256, 384, 512 ]
 var hash = function (bitcount) {
   if (bitcount !== undefined && hashLengths.indexOf(bitcount) == -1)
     throw new Error('Unsupported hash length')
-  this.content = ''
+  this.content = []
   this.bitcount = bitcount ? 'keccak_' + bitcount : 'keccak_512'
 }
 
 hash.prototype.update = function (i) {
   if (Buffer.isBuffer(i))
-    this.content = i
+    this.content.push(i)
   else if (typeof i === 'string')
-    this.content = new Buffer(i)
+    this.content.push(new Buffer(i))
   else
     throw new Error('Unsupported argument to update')
   return this
 }
 
 hash.prototype.digest = function (encoding) {
-  var result = Sha3[this.bitcount](this.content)
+  var result = Sha3[this.bitcount](Buffer.concat(this.content))
   if (encoding === 'hex')
     return result
   else if (encoding === 'binary' || encoding === undefined)

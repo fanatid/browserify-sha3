7763f19bbb229e78835b04b91fb208a8f718027f
Kirill Fomichev <fanatid@ya.ru>
2016-03-25 16:53:12 +0400

    `.digest` return `Buffer` by default

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const Sha3 = require('js-sha3')
+var Sha3 = require('js-sha3')
 
-const hashLengths = [ 224, 256, 384, 512 ]
+var HASH_LENGTS = [ 224, 256, 384, 512 ]
 
-var hash = function (bitcount) {
-  if (bitcount !== undefined && hashLengths.indexOf(bitcount) == -1)
-    throw new Error('Unsupported hash length')
-  this.content = []
-  this.bitcount = bitcount ? 'keccak_' + bitcount : 'keccak_512'
+function SHA3Hash (bitcount) {
+  if (bitcount !== undefined) {
+    if (HASH_LENGTS.indexOf(bitcount) == -1) throw new Error('Unsupported hash length')
+    bitcount = 512
+  }
+
+  this._content = []
+  this._bitcount = 'keccak_' + bitcount
 }
 
-hash.prototype.update = function (i) {
-  if (Buffer.isBuffer(i))
-    this.content.push(i)
-  else if (typeof i === 'string')
-    this.content.push(new Buffer(i))
-  else
+SHA3Hash.prototype.update = function (data, encoding) {
+  if (Buffer.isBuffer(data)) {
+    this._content.push(data)
+  } else if (typeof data === 'string') {
+    this._content.push(new Buffer(data, encoding))
+  } else {
     throw new Error('Unsupported argument to update')
+  }
+
   return this
 }
 
-hash.prototype.digest = function (encoding) {
-  var result = Sha3[this.bitcount](Buffer.concat(this.content))
-  if (encoding === 'hex')
-    return result
-  else if (encoding === 'binary' || encoding === undefined)
-    return new Buffer(result, 'hex').toString('binary')
-  else
-    throw new Error('Unsupported encoding for digest: ' + encoding)
+SHA3Hash.prototype.digest = function (encoding) {
+  var result = Sha3[this._bitcount](Buffer.concat(this._content))
+  if (encoding === 'hex') return result
+
+  result = new Buffer(result, 'hex')
+  if (encoding !== undefined) result = result.toString('encoding')
+  return result
 }
 
-module.exports = {
-  SHA3Hash: hash
-}
+module.exports.SHA3Hash = SHA3Hash
